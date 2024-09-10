95c2071cb420ee3f3a4a177943e65334c6abcca6
chrisewoi <69186935+chrisewoi@users.noreply.github.com>
2024-09-10 17:07:22 +1000

    Research first row complete

--- Assets/Upgrades/DiverSpeedUpgrade.cs
+++ Assets/Upgrades/DiverSpeedUpgrade.cs
@@ -1,14 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DiverSpeedUpgrade : MonoBehaviour
 {
+    public TextMeshProUGUI text;
+    public int researchTier => RestaurantParameters.ins.DiverResearchTier;
+    public int maxResearchTier => RestaurantParameters.ins.DiverMaxResearchTier;
+    public float diverRunningSpeed => RestaurantParameters.ins.DiverRunningSpeed;
+    public float diverSpeedIncrease => RestaurantParameters.ins.DiverSpeedIncrease;
+    public int cost => RestaurantParameters.ins.DiverResearchCost;
+    public bool upgradable => researchTier < maxResearchTier;
 
-    public float diverSpeedIncrease = 0.2f;
+
+    void Start()
+    {
+        SpendResearch spendResearch = GetComponent<SpendResearch>();
+        spendResearch.AddListener(OnUpgrade);
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        UpdateText();
+    }
     public void OnUpgrade()
     {
-        RestaurantParameters.ins.DiverRunningSpeed += diverSpeedIncrease;
+        if (cost < RestaurantParameters.ins.ResearchPointsGained && upgradable)
+        {
+            RestaurantParameters.ins.ResearchPointsGained -= cost;
+            RestaurantParameters.ins.DiverRunningSpeed += diverSpeedIncrease;
+            RestaurantParameters.ins.DiverResearchTier++;
+            UpdateText();
+        }
     }
-
+    void UpdateText()
+    {
+        text.text = researchTier + "/" + maxResearchTier + "\nCurrent: " + Mathf.Round(diverRunningSpeed * 100f) / 100f;
+        if (upgradable)
+        {
+            text.text = text.text + "\nNext: " + Mathf.Round((diverRunningSpeed + diverSpeedIncrease) * 100f) / 100f;
+        }
+    }
 }
--- Assets/Upgrades/DockworkerSpeedUpgrade.cs
+++ Assets/Upgrades/DockworkerSpeedUpgrade.cs
@@ -1,14 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class DockworkerSpeedUpgrade : MonoBehaviour
 {
+    public TextMeshProUGUI text;
+    public int researchTier => RestaurantParameters.ins.DockWorkerResearchTier;
+    public int maxResearchTier => RestaurantParameters.ins.DockWorkerMaxResearchTier;
+    public float dockWorkerRunningSpeed => RestaurantParameters.ins.DockWorkerRunningSpeed;
+    public float dockWorkerSpeedIncrease => RestaurantParameters.ins.DockWorkerSpeedIncrease;
+    public int cost => RestaurantParameters.ins.DockWorkerResearchCost;
+    public bool upgradable => researchTier < maxResearchTier;
 
-    public float dockworkerSpeedIncrease = 0.2f;
+
+    void Start()
+    {
+        SpendResearch spendResearch = GetComponent<SpendResearch>();
+        spendResearch.AddListener(OnUpgrade);
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        UpdateText();
+    }
     public void OnUpgrade()
     {
-        RestaurantParameters.ins.DockWorkerRunningSpeed += dockworkerSpeedIncrease;
+        if (cost < RestaurantParameters.ins.ResearchPointsGained && upgradable)
+        {
+            RestaurantParameters.ins.ResearchPointsGained -= cost;
+            RestaurantParameters.ins.DockWorkerRunningSpeed += dockWorkerSpeedIncrease;
+            RestaurantParameters.ins.DockWorkerResearchTier++;
+            UpdateText();
+        }
     }
-
+    void UpdateText()
+    {
+        text.text = researchTier + "/" + maxResearchTier + "\nCurrent: " + Mathf.Round(dockWorkerRunningSpeed * 100f) / 100f;
+        if (upgradable)
+        {
+            text.text = text.text + "\nNext: " + Mathf.Round((dockWorkerRunningSpeed + dockWorkerSpeedIncrease) * 100f) / 100f;
+        }
+    }
 }
